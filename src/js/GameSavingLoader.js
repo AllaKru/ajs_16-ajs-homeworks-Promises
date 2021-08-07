80628bb10a56fa10b95f8f383a6f6bb7b1679380
Alla Kruglova <renata-l@yandex.ru>
2021-08-07 16:13:47 +0300

    first commit

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,12 @@
+import json from './parser';
+import read from './reader';
+
+
+export default class GameSavingLoader {
+  static load() {
+    // const data = read(); // возвращается Promise!
+    // const value = json(data); // возвращается Promise!
+    // eslint-disable-next-line no-console
+    return read().then((response) => json(response)).catch((error) => console.log(error));
+  }
+}
